e04e94a1fbbb59b82c764442db4c8635d774b807
Vinit <vinits1014@gmail.com>
2025-03-23 13:50:38 +0530

    Added navbar section to it

---
+++ frontend/Directives/navbar/navbar.directive.js
@@ -0,0 +1,22 @@
+"use strict";
+angular.module("gst_invoice").directive("navbar", function ($document) {
+  return {
+    restrict: "E",
+    templateUrl: "./Directives/navbar/navbar.html",
+    controller: function ($scope) {
+      $scope.menuOpen = false;
+
+      // Logo Image URL
+      $scope.logoUrl =
+        "https://media.licdn.com/dms/image/v2/D5622AQE9zq12SHD6sA/feedshare-shrink_800/B56ZUY4KjhHQAg-/0/1739879136122?e=2147483647&v=beta&t=3gNRg4sDzYd7XY2dgYOkZ2VrnKTrTmkQn1QdrxHnD10";
+
+      // Toggle Mobile Menu
+      $scope.toggleMenu = function () {
+        $scope.menuOpen = !$scope.menuOpen;
+        // console.log("object");
+      };
+
+      // Close menu when clicking outside
+    },
+  };
+});
