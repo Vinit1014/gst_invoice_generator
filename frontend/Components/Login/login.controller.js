8a75a7795bb00b1cfad3e71c0bf3363ee72812d1
Vinit <vinits1014@gmail.com>
2025-03-23 10:57:30 +0530

    Added register and login frontend component

--- frontend/Components/Login/login.controller.js
+++ frontend/Components/Login/login.controller.js
@@ -2,7 +2,40 @@
 
 angular
   .module("gst_invoice")
-  .controller("loginController", function ($scope) {
-    $scope.test8 = "Hello login";
-});
+  .controller("loginController", function ($rootScope, $scope, $http, $location) {
+    $scope.email = "";
+    $scope.password = "";
+    
+    // Submit handler
+    $scope.onSubmit = function (loginForm) {
+      if (loginForm.$valid) {
+        const postData = {
+          email: $scope.email,
+          password: $scope.password,
+        };
 
+        $http
+          .post("http://localhost/gst-invoice-generator/index.php/login", postData)
+          .then(function (response) {
+            console.log("Response from backend:", response.data);
+            if (response.data.status == "success") {
+              alert("login successful!");
+              console.log(response);
+              localStorage.setItem("user_id", response.data.user.id);
+              $rootScope.userData = response.data.user.id;
+              $location.path("/dashboard");
+            } else {
+              alert("Error: " + response.data.message);
+            }
+          })
+          .catch(function (error) {
+            console.error("Error connecting to backend:", error);
+            alert("An error occurred while login. Please try again.");
+          });
+        loginForm.$setPristine();
+        loginForm.$setUntouched();
+      } else {
+        console.log("Invalid Form");
+      }
+    };
+});
--- frontend/Components/Register/register.controller.js
+++ frontend/Components/Register/register.controller.js
@@ -1,8 +1,26 @@
-"use strict";
 
 angular
   .module("gst_invoice")
-  .controller("registerController", function ($scope) {
-    $scope.test7 = "Hello register";
-});
+  .controller("registerController", function ($scope, $http, $location) {
+    $scope.name = "";
+    $scope.email = "";
+    $scope.pNumber = "";
+    $scope.role = "";
+    $scope.password = "";
 
+    $scope.onSubmit = function (signupForm) {
+      if (signupForm.$valid) {
+        // Prepare data to send to backend
+        const postData = {
+          name: $scope.name,
+          email: $scope.email,
+          phone: $scope.pNumber,
+          role: $scope.role,
+          password: $scope.password,
+        };
+        console.log("Form Submitted", postData);
+
+        
+      }
+    };
+});
